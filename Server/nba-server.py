745af830606c4ce423a623def53f39cec1861ca1
orya <orya.sp@gmail.com>
2022-10-04 16:32:38 +0300

    Added search path to my-nba-server.py

--- Server/nba-server.py
+++ Server/nba-server.py
@@ -3,7 +3,9 @@ import uvicorn
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import requests
+import os
 
+from nba_api import NbaAPI
 
 app = FastAPI()
 
@@ -16,18 +18,32 @@ app = FastAPI()
 # example - localhost:8000/static/view.jpg
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/Client", StaticFiles(directory="Client"), name="Client")
 
 
 
 @app.get('/')
 def root(response: Response):
-    
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    api = NbaAPI("lakers", 2018)
+    print(api.headers)
     return {"message":"Server is up and running"}
 
 
-# @app.get('/search')
-# def get_players():
-#     return {"msg":"Players"}
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    path = "C:/Users/Rent/Desktop/NBA/Client/static/favicon.ico"
+    return FileResponse(path)
+    # return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
+
+
+@app.get('/search')
+def get_players():
+    data = NbaAPI("warriors","2018").get_data()
+    return data
+    # return {"msg":"Players"}
+
+
 
 
 
