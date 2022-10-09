15c4943ea2a473981b4f76f0f00e467e26cb6e76
orya <orya.sp@gmail.com>
2022-10-09 11:42:17 +0300

    Added metadata ajax call to client

---
+++ Client/static/api/metadata.js
@@ -0,0 +1,11 @@
+class MetaDataApi extends Api {
+
+	constructor(year = "2018", team = "lakers", apiInterface = new AjaxCall()) {
+		let url = `http://localhost:8000/search?teamName=${team}&year=${year}`
+		super(apiInterface, url)
+	}
+
+	async getData() {
+		return await this.callApi()
+	}
+}
