9c07ef436e186499e70edc5a9cf7d2caca095be0
orya <orya.sp@gmail.com>
2022-10-09 16:55:16 +0300

    Added pageController.js

---
+++ Client/static/pageController.js
@@ -0,0 +1,5 @@
+/* Controller
+Guidelines for the controller:
+Use the instances of your classes with MVC principles so that when the user clicks the generate user button, 
+it will fetch and load the data on the screen  */
+
