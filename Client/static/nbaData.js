fd615b82ebc4d599d811e8cabe274b9c9f685495
orya <orya.sp@gmail.com>
2022-10-09 16:55:29 +0300

    Added nbaData.py

---
+++ Client/static/nbaData.js
@@ -0,0 +1,6 @@
+/* Data Model
+This model is in charge of handling all data related functionalities, it should make all the API's request 
+and able to generate a user’s data.  */
+
+players = MetaDataApi().getData()
+print("hi")
