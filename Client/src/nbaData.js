cc0a60a58ae59d82e25bdcda106f04fdfc7cd18c
orya <orya.sp@gmail.com>
2022-10-12 22:37:55 +0300

    Added nbaData module to nbaData.js

--- Client/src/nbaData.js
+++ Client/src/nbaData.js
@@ -2,5 +2,25 @@
 This model is in charge of handling all data related functionalities, it should make all the API's request 
 and able to generate a user’s data.  */
 
-let players = new MetaDataApi().getData()
-print("hi")
+
+const nbaData = function() {
+    
+    let players; 
+
+    function init(year, team) {
+        players = new MetaDataApi(year, team)
+    }
+
+    async function getData() {
+        let nbaPromise = players.getData() 
+        return await Promise.all([nbaPromise]).then(function(res) {
+            console.log(res[0]);    ///////////
+            return {metaData: res[0]}
+        }) 
+    }
+
+    return {
+        init,
+        getData
+    }
+}
