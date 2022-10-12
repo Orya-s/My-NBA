273c5e80cf9aaa451d55879d08c4c27a15204ef9
orya <orya.sp@gmail.com>
2022-10-12 22:35:12 +0300

    Fixed printing in metadata.js

--- Client/src/api/metadata.js
+++ Client/src/api/metadata.js
@@ -6,7 +6,7 @@ class MetaDataApi extends Api {
 	}
 
 	async getData() {
-        print("performing metadata call")
+        console.log("performing metadata call");
 		return await this.callApi()
 	}
 }
