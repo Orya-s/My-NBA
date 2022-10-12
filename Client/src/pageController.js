2dbe7b47eea22237f3ad9080280387de9c8fd9c2
orya <orya.sp@gmail.com>
2022-10-12 22:37:27 +0300

    Added on-click event to pageController.js

--- Client/src/pageController.js
+++ Client/src/pageController.js
@@ -3,3 +3,29 @@ Guidelines for the controller:
 Use the instances of your classes with MVC principles so that when the user clicks the generate user button, 
 it will fetch and load the data on the screen  */
 
+const nbaPlayers = nbaData()
+const rendPage = nbaRender()
+
+$("#getTeamBtn").on("click", function() {
+    const year = $("#yearInput").val()
+    const team = $("#teamInput").val()
+    console.log(team + " " + year);     //////////
+    
+    if (team == "") {
+        console.warn("team name is missing from input")
+        alert("Please enter a team name!")
+    }
+    else {
+        nbaPlayers.init(year, team);
+        nbaPlayers.getData().then((res) => {
+            cleanInput()
+            rendPage.renderPlayers(res)
+            return res
+        })
+    }
+})
+
+const cleanInput = function() {
+    $("#yearInput").val("")
+    $("#teamInput").val("")
+}
