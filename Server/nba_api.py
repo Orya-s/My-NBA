b8b0574a56e1dde6ddedbf85e49e28404a2b9753
orya <orya.sp@gmail.com>
2022-10-13 17:53:09 +0300

    Fixed get(team) on nba_api.py

--- Server/nba_api.py
+++ Server/nba_api.py
@@ -13,8 +13,8 @@ class NbaAPI(API):
     def __init__(self, team_name, year):
         self.url = f"http://data.nba.net/10s/prod/v1/{year}/players.json"
         super().__init__(self.url)
+        self.team = teams_id.get(team_name, None)
         self.year = year
-        self.team = teams_id[team_name]
         self.headers = {"Content-Type": "application/json"}
         
     
